49a0dacbb99bdf18477e9b0ff317fae47603e6b6
Piotr Kubala <piotr.kubala@o2.pl>
2023-12-27 13:35:50 +0100

    history filter

---
+++ lab9/BiuroTurystyczne/src/app/shopping-history/history-filter.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { HistoryFilterPipe } from './history-filter.pipe';
+
+describe('HistoryFilterPipe', () => {
+  it('create an instance', () => {
+    const pipe = new HistoryFilterPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
--- lab9/BiuroTurystyczne/src/app/shopping-history/shopping-history.component.ts
+++ lab9/BiuroTurystyczne/src/app/shopping-history/shopping-history.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import {ShoppingHistoryService} from "../shopping-history.service";
 import {NgForOf, NgIf} from "@angular/common";
 import {ShoppingItemComponent} from "./shopping-item/shopping-item.component";
+import {HistoryItemState} from "../history-item-state";
+import {HistoryFilterPipe} from "./history-filter.pipe";
 
 @Component({
   selector: 'app-shopping-history',
@@ -9,15 +11,28 @@ import {ShoppingItemComponent} from "./shopping-item/shopping-item.component";
   imports: [
     NgForOf,
     NgIf,
-    ShoppingItemComponent
+    ShoppingItemComponent,
+    HistoryFilterPipe
   ],
   templateUrl: './shopping-history.component.html',
   styleUrl: './shopping-history.component.css'
 })
 export class ShoppingHistoryComponent {
+  protected readonly HistoryItemState = HistoryItemState;
+  isSortingAscending = true;
+  statusFilter: HistoryItemState | null = null;
+
   constructor(public shoppingHistoryService: ShoppingHistoryService) {}
 
   isShoppingHistoryEmpty() {
     return this.shoppingHistoryService.isShoppingHistoryEmpty();
+  }
+
+  filterVisibleByStatus(status: HistoryItemState | null) {
+    this.statusFilter = status;
+  }
+
+  setSortingOrder(isAscending: boolean) {
+    this.isSortingAscending = isAscending;
   }
 }
